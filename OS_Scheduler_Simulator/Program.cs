92ea2594cb7b9ce22b94ec4f68845d2cb5718abb
AmirHesari <amir.hesari1848@gmail.com>
2022-01-14 20:21:38 +0330

    'init'

---
+++ OS_Scheduler_Simulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OS_Scheduler_Simulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
